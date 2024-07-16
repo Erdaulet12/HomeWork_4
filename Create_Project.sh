b16429f571a34a5ba7287d08bde7cdebf8c2a772
Erdaulet12 <erdauletakilzhan83@gmail.com>
2024-07-16 21:43:02 +0500

    Создание проекта

---
+++ Create_Project.sh
@@ -0,0 +1,15 @@
+django-admin startproject MyProject
+
+cd MyProject
+
+python manage.py startapp MyApp
+
+python -m venv venv
+
+venv/Scripts/activate
+
+pip install django
+
+pip freeze > requirements.txt
+
+echo "Project was created"
